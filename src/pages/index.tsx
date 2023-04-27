c22847c921c5582497e82ee0cbb73f1512cf07a6
jainaldo <jainaldosilva@gmail.com>
2023-04-27 20:00:51 -0300

    Componente: Banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Banner } from "@/components/Banner";
 import { Header } from "@/components/Header";
 import { Flex } from "@chakra-ui/react";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <Flex direction="column" h="100vh">
       <Header />
+      <Banner />
     </Flex>
   )
 }
