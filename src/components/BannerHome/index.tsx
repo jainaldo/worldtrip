224f91dc73ba3b8620dd74df93c587faf401c568
jainaldo <jainaldosilva@gmail.com>
2023-04-27 21:32:35 -0300

    Terminando a página home

--- src/components/BannerHome/index.tsx
+++ src/components/BannerHome/index.tsx
@@ -1,16 +1,16 @@
 import { Box, Flex, Image, Text } from "@chakra-ui/react";
 
-export function Banner() {
+export function BannerHome() {
 
   return (
     <Flex width="100%" height="335px" bgImage="./images/Background.svg" backgroundSize={"cover"} backgroundRepeat={"no-repeat"}>
-      <Flex width="1160px" align="center" justify="space-between" mx="auto" my="0">
+      <Flex width="1160px" align="center" height="368.21px" justify="space-between" mx="auto" my="0">
         <Box>
           <Text fontWeight="500" width="426px" fontSize="4xl">5 Continentes, infinitas possibilidades.</Text>
           <Text fontWeight="400" mt="5" width="524px" fontSize="xl" color="gray.300">Chegou a hora de tirar do papel a viagem que você sempre sonhou.</Text>
         </Box>
         <Image
-          mt="28"
+          mt="24"
           src="./images/Airplane.svg" 
           alt="imagem de aviação"/>
       </Flex>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,81 @@
-import { Banner } from "@/components/Banner";
+import { BannerHome } from "@/components/BannerHome";
 import { Header } from "@/components/Header";
-import { Flex } from "@chakra-ui/react";
+import { Box, Divider, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import Image from "next/image";
 
 
 export default function Home() {
   return (
     <Flex direction="column" h="100vh">
       <Header />
-      <Banner />
+      <BannerHome />
+      <Box w="100%" my={["16","24"]} maxWidth={1160} mx="auto">
+        <SimpleGrid gap="4" minChildWidth="145px">
+          <Flex direction="column" align="center" justify="center">
+            <Image src="./images/cocktail.svg" width="85" height="85" alt="imagem de tarça de bebida"/>
+            <Text 
+              fontSize="2xl"
+              fontWeight="600"
+              mt="6"
+              color="gray.700"
+            >
+              vida noturna
+            </Text>
+          </Flex>
+          <Flex direction="column" align="center" justify="center">
+            <Image src="./images/surf.svg" width="85" height="85" alt="imagem de tarça de bebida"/>
+            <Text 
+              fontSize="2xl"
+              fontWeight="600"
+              mt="6"
+              color="gray.700"
+            >
+              praia
+            </Text>
+          </Flex>
+          <Flex direction="column" align="center" justify="center">
+            <Image src="./images/building.svg" width="85" height="85" alt="imagem de tarça de bebida"/>
+            <Text 
+              fontSize="2xl"
+              fontWeight="600"
+              mt="6"
+              color="gray.700"
+            >
+              moderno
+            </Text>
+          </Flex>
+          <Flex direction="column" align="center" justify="center">
+            <Image src="./images/museum.svg" width="85" height="85" alt="imagem de tarça de bebida"/>
+            <Text 
+              fontSize="2xl"
+              fontWeight="600"
+              mt="6"
+              color="gray.700"
+            >
+              clássico
+            </Text>
+          </Flex>
+          <Flex direction="column" align="center" justify="center">
+            <Image src="./images/earth.svg" width="85" height="85" alt="imagem de tarça de bebida"/>
+            <Text 
+              fontSize="2xl"
+              fontWeight="600"
+              mt="6"
+              color="gray.700"
+            >
+              e mais...
+            </Text>
+          </Flex>
+        </SimpleGrid>
+      </Box>
+      
+      <Flex align="center" justify="center" direction="column" >
+        <Divider borderColor="gray.700" width={"90px"} border="2"/>
+        <Flex my="14" align="center" justify="center" direction="column" >
+          <Text color="gray.700" fontWeight="500" fontSize="xl">Vamos nessa?</Text>
+          <Text color="gray.700" fontWeight="500" fontSize="xl">Então escolha seu continente</Text>
+        </Flex>
+      </Flex>
     </Flex>
   )
 }
